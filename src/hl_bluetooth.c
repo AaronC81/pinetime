dc6b825d5ac0333ed8f5d77030a6f2a6dae9f13f
Ezekiel Bethel <zek@9net.org>
2020-01-14 22:31:05 +0000

    add l o n g vendor characteristic

--- src/hl_bluetooth.c
+++ src/hl_bluetooth.c
@@ -8,7 +8,72 @@
 #include <bluetooth/services/bas.h>
 #include <bluetooth/services/hrs.h>
 
+void bluetooth_debug_notify(char *str);
 struct bt_conn *_hl_bluetooth_default_conn;
+
+static struct bt_uuid_128 vnd_uuid = BT_UUID_INIT_128(
+	0xf0, 0xde, 0xbc, 0x9a, 0x78, 0x56, 0x34, 0x12,
+	0x78, 0x56, 0x34, 0x12, 0x78, 0x56, 0x34, 0x12);
+
+#define MAX_DATA 74
+static u8_t vnd_long_value[] = {
+		  'V', 'e', 'n', 'd', 'o', 'r', ' ', 'd', 'a', 't', 'a', '1',
+		  'V', 'e', 'n', 'd', 'o', 'r', ' ', 'd', 'a', 't', 'a', '2',
+		  'V', 'e', 'n', 'd', 'o', 'r', ' ', 'd', 'a', 't', 'a', '3',
+		  'V', 'e', 'n', 'd', 'o', 'r', ' ', 'd', 'a', 't', 'a', '4',
+		  'V', 'e', 'n', 'd', 'o', 'r', ' ', 'd', 'a', 't', 'a', '5',
+		  'V', 'e', 'n', 'd', 'o', 'r', ' ', 'd', 'a', 't', 'a', '6',
+		  '.', ' ' };
+
+static ssize_t read_long_vnd(struct bt_conn *conn,
+			     const struct bt_gatt_attr *attr, void *buf,
+			     u16_t len, u16_t offset)
+{
+	const char *value = attr->user_data;
+
+	return bt_gatt_attr_read(conn, attr, buf, len, offset, value,
+				 sizeof(vnd_long_value));
+}
+
+static ssize_t write_long_vnd(struct bt_conn *conn,
+			      const struct bt_gatt_attr *attr, const void *buf,
+			      u16_t len, u16_t offset, u8_t flags)
+{
+	u8_t *value = attr->user_data;
+
+	if (flags & BT_GATT_WRITE_FLAG_PREPARE) {
+		return 0;
+	}
+
+	if (offset + len > sizeof(vnd_long_value)) {
+		return BT_GATT_ERR(BT_ATT_ERR_INVALID_OFFSET);
+	}
+
+	memcpy(value + offset, buf, len);
+
+	return len;
+}
+
+static const struct bt_uuid_128 vnd_long_uuid = BT_UUID_INIT_128(
+	0xf3, 0xde, 0xbc, 0x9a, 0x78, 0x56, 0x34, 0x12,
+	0x78, 0x56, 0x34, 0x12, 0x78, 0x56, 0x34, 0x12);
+
+static struct bt_gatt_cep vnd_long_cep = {
+	.properties = BT_GATT_CEP_RELIABLE_WRITE,
+};
+
+/* Vendor Primary Service Declaration */
+BT_GATT_SERVICE_DEFINE(vnd_svc,
+	BT_GATT_PRIMARY_SERVICE(&vnd_uuid),
+	//BT_GATT_CCC(vnd_ccc_cfg_changed,
+	//	    BT_GATT_PERM_READ | BT_GATT_PERM_WRITE_ENCRYPT),
+	BT_GATT_CHARACTERISTIC(&vnd_long_uuid.uuid, BT_GATT_CHRC_READ |
+			       BT_GATT_CHRC_WRITE | BT_GATT_CHRC_EXT_PROP,
+			       BT_GATT_PERM_READ | BT_GATT_PERM_WRITE |
+			       BT_GATT_PERM_PREPARE_WRITE,
+			       read_long_vnd, write_long_vnd, &vnd_long_value),
+	BT_GATT_CEP(&vnd_long_cep),
+);
 
 // The data used to advertise this device over Bluetooth.
 static const struct bt_data _hl_ad_data[] = {
@@ -22,6 +87,9 @@ static const struct bt_data _hl_ad_data[] = {
     //   - 0x180A: Device information
     // https://www.bluetooth.com/specifications/gatt/services/
 	BT_DATA_BYTES(BT_DATA_UUID16_ALL, 0x0d, 0x18, 0x0f, 0x18, 0x0a, 0x18),
+	BT_DATA_BYTES(BT_DATA_UUID128_ALL,
+		      0xf0, 0xde, 0xbc, 0x9a, 0x78, 0x56, 0x34, 0x12,
+		      0x78, 0x56, 0x34, 0x12, 0x78, 0x56, 0x34, 0x12),
 };
 
 // Callback invoked when Bluetooth connects.
@@ -30,6 +98,8 @@ void _hl_bluetooth_connected(struct bt_conn *conn, u8_t err)
 	if (!err) {
 		_hl_bluetooth_default_conn = bt_conn_ref(conn);
 	}
+
+	bluetooth_debug_notify("bruh moment");
 }
 
 // Callback invoked when Bluetooth disconnects.
@@ -58,6 +128,12 @@ void hl_bluetooth_bas_notify(uint8_t battery_level)
 void hl_bluetooth_hrs_notify(uint16_t heart_rate)
 {
 	bt_gatt_hrs_notify(heart_rate);
+}
+
+void bluetooth_debug_notify(char *str)
+{
+	strncpy(vnd_long_value, str, MAX_DATA);
+	bt_gatt_notify(_hl_bluetooth_default_conn, &vnd_svc.attrs[0], vnd_long_value, MAX_DATA);
 }
 
 // Opens this Bluetooth device for connections.
